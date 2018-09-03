50a6dc10027db3af9471aad6956ca5f03e504529
Ezequiel <ecohort@fjsolutions.com.ar>
2018-09-03 11:16:05 -0300

    Ultimas modificaciones Estilos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
+import AppBar from 'material-ui/AppBar';
 import LocationList from './components/LocationList';
 
 import './App.css';
@@ -18,12 +21,26 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <div className="App">
-          <LocationList 
-              cities={cities} 
-              onSelectedLocation={this.handleSelectedLocation}
-          ></LocationList>
-        </div>
+        <Grid>
+          <Row>
+            <Col xs={12}>
+              <AppBar title="Weather App" />
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} md={6}>
+              <LocationList 
+                  cities={cities} 
+                  onSelectedLocation={this.handleSelectedLocation}
+              ></LocationList>
+            </Col>
+            <Col xs={12} md={6}>
+              <Paper zDepth={4}>
+                <div className="detail" ></div>
+              </Paper>
+            </Col>
+          </Row>
+        </Grid>
       </MuiThemeProvider>
     );
   }
